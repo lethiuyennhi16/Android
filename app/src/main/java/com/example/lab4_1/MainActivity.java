c61cbe55b66800c6fc1139512eae0c2e3de1ea49
lethiuyennhi16 <ltun20031605@gmail.com>
2025-04-22 21:24:23 +0700

    Circle layout

---
+++ app/src/main/java/com/example/lab4_1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lab4_1;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.TaskStackBuilder;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
